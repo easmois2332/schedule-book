1c207c98e388426e161f244af1e90cb8cbb57402
easmois2332 <easmois2332@gmail.com>
2025-01-24 02:26:09 +0900

    add idol const

--- src/views/IdolView.vue
+++ src/views/IdolView.vue
@@ -1,6 +1,12 @@
 <script setup>
+import {idols, produces} from "@/consts/idolConst";
+
+for (let index in produces) {
+  let produce = produces[index];
+  console.log(produce);
+}
 </script>
 
 <template>
-  <h1>idol page</h1>
+  <h1>Pアイドル一覧</h1>
 </template>
