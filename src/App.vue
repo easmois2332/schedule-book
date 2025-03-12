4ff5bca4b06e6d1cb1d153636f7d0b39e7d5c1ed
easmois2332 <easmois2332@gmail.com>
2025-03-12 14:44:35 +0900

    phone design for header tab

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,8 @@ const buttonIdol = () => {
 const buttonCard = () => {
   currentComponent.value = SupportCardView
 }
+const buttonCalculator = () => {
+}
 const buttonSetting = () => {
 }
 </script>
@@ -72,6 +74,14 @@ const buttonSetting = () => {
               <path d="M560-440h200v-80H560v80Zm0-120h200v-80H560v80ZM200-320h320v-22q0-45-44-71.5T360-440q-72 0-116 26.5T200-342v22Zm160-160q33 0 56.5-23.5T440-560q0-33-23.5-56.5T360-640q-33 0-56.5 23.5T280-560q0 33 23.5 56.5T360-480ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm0-80h640v-480H160v480Zm0 0v-480 480Z"/>
             </svg>
             <span class="header-tab-name">サポートカード</span>
+          </button>
+        </div>
+        <div class="header-tab calculator" @click="buttonCalculator">
+          <button class="header-tab-button calculator">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+              <path d="M320-240h60v-80h80v-60h-80v-80h-60v80h-80v60h80v80Zm200-30h200v-60H520v60Zm0-100h200v-60H520v60Zm44-152 56-56 56 56 42-42-56-58 56-56-42-42-56 56-56-56-42 42 56 56-56 58 42 42Zm-314-70h200v-60H250v60Zm-50 472q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/>
+            </svg>
+            <span class="header-tab-name">計算機</span>
           </button>
         </div>
         <div class="header-tab setting" @click="buttonSetting">
